8af17c3122415aa803b6788d5305e977b62d3a35
gjergjk71 <gjergjk71@gmail.com>
2018-10-12 23:46:07 +0200

    Created shoot method

--- js/main.js
+++ js/main.js
@@ -13,13 +13,20 @@ class Player_SpaceShip{
 		this.h = h;
 		this.speed = speed;
 		this.dt = dt;
+		this.lasers = []
 	}
 	get draw(){
 		this.reDraw();
 	}
 	reDraw(){
+		var laser_image = document.getElementById("laser_image");
 		this.ctx.beginPath();
 		this.ctx.drawImage(this.image,this.x,this.y,this.w,this.h);
+		for (var i in this.lasers){
+			var laser = this.lasers[i];	
+			this.ctx.drawImage(laser_image,laser.x,laser.y,laser.w,laser.h);
+			this.lasers[i].y -= laser.speed * this.dt;
+		}
 	}
 	get move(){
 		this.move_player();
@@ -28,18 +35,25 @@ class Player_SpaceShip{
 		if (keyState["ArrowUp"] || keyState["w"]){
 			this.y -= this.speed * this.dt;
 		}
-		if (keyState["ArrowDown"] || keyState["s"]){
-			this.y += this.speed * this.dt;
-		}
 		if (keyState["ArrowLeft"] || keyState["a"]){
 			this.x -= this.speed * this.dt;
+		}
+		if (keyState["ArrowDown"] || keyState["s"]){
+			this.y += this.speed * this.dt;
 		}
 		if (keyState["ArrowRight"] || keyState["d"]){
 			this.x += this.speed * this.dt;
 		}
+		if (keyState[" "]) {
+			this.shoot();
+		}
+	}
+	shoot(){
+		var middle = this.x + this.w - (this.w / 2) - 5;
+		this.lasers.push({"x":middle,"y":this.y - 15,"w":10,"h":30,"speed":1000});
 	}
 }
-player = new Player_SpaceShip(ctx,10,10,100,100,100);
+player = new Player_SpaceShip(ctx,100,200,100,100,100);
 keyState = {};
 
 document.addEventListener("keydown", (event) => {
