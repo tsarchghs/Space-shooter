e38b1507e5af36f54bebc53ba370226bfd4d29de
gjergjk71 <gjergjk71@gmail.com>
2018-10-14 17:41:16 +0200

    Created Button class

---
+++ js/ui/button.js
@@ -0,0 +1,30 @@
+class Button{
+	constructor(ctx,x,y,w,h,button_image_url){
+		this.ctx = ctx;
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.button_image = new Image();
+		this.button_image.src = button_image_url;
+		this.screenX;
+		this.screenY;
+		this.mousePressed;
+	}
+	get draw(){
+		this.reDraw();
+	}
+	reDraw(){
+		this.ctx.beginPath();
+		this.ctx.drawImage(this.button_image,this.x,this.y,this.w,this.h);
+		if (this.screenX && this.screenY){
+			if (this.x <= this.screenX && this.screenX <= this.x + this.w &&
+				this.y <= this.screenY && this.screenY <= this.y + this.h){
+				this.pressed();
+			}
+		}
+	}
+	pressed(){
+		console.log("pressed");
+	}
+}
