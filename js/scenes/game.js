fd1ec58c0ab613718ba9008b1906e86293f704a0
gjergjk71 <gjergjk71@gmail.com>
2018-10-16 22:04:01 +0200

    Optimized for loop for asteroids

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -44,18 +44,22 @@ class Game{
 			this.health.draw;
 		}
 		this.score.draw;
-		for (var type_pickup in this.pickups){ // type_pickup means [0] for type pickup for [1]
+		for (var i in this.pickups){ // type_pickup means [0] for type pickup for [1]
+			var type_pickup = this.pickups[i];
 			if (this.dt){
-				this.pickups[type_pickup][1].dt = this.dt;
-				this.pickups[type_pickup][1].draw;
+				this.pickups[i][1].dt = this.dt;
+				this.pickups[i][1].draw;
 			}
 			CollisionDetector.rect1 = this.player;
-			CollisionDetector.rect2 = this.pickups[type_pickup][1];
+			CollisionDetector.rect2 = this.pickups[i][1];
 			if (CollisionDetector.get_rect_rect_collision){
+				console.log(type_pickup);
 				if (type_pickup[0] == "HealthPickup"){
 					this.health.health += 1;
+				} else if (type_pickup[0] == "powerUp_pickup"){
+					this.player.get_level_up;
 				}
-				delete this.pickups[type_pickup];
+				delete this.pickups[i];
 			}
 		}
 		if (!this.health.health){
@@ -80,26 +84,25 @@ class Game{
 						CollisionDetector.rect2 = laser;
 						if (CollisionDetector.get_rect_rect_collision){
 							this.asteroids[asteroid].health -= this.player.shoot_damage;
-							if (!this.asteroids[asteroid].health  && this.health.health){
+							if (this.asteroids[asteroid].health <= 0  && this.health.health){
 								this.score.score += 100;
+								this.asteroids[asteroid].destroyed = true;
 							}
 							this.player.lasers.splice(i,1);
 						}
 					}
 				}
+				asteroid = this.asteroids[asteroid];
+		    	asteroid.draw;
+		    	asteroid.y += asteroid.speed * this.dt;
+		    	if (asteroid.y > canvas.height){
+		    		if (this.health.health && !asteroid.destroyed) {
+		    			this.health.health -= 1;
+		    		}
+		    		this.asteroids.splice(asteroid,1);
+		    	}
 			}
 		}
-	    for (var i in this.asteroids){
-	    	asteroid = this.asteroids[i];
-	    	asteroid.draw;
-	    	asteroid.y += asteroid.speed * this.dt;
-	    	if (asteroid.y > canvas.height){
-	    		if (this.health.health && !asteroid.destroyed) {
-	    			this.health.health -= 1;
-	    		}
-	    		this.asteroids.splice(asteroid,1);
-	    	}
-	    }
 		createAsteroid(this.dt,score.score);
 		createHealthPickUp(this.dt);
 		createPowerUpPickUp(this.dt);
