c26be50a9a7e7ed4bd1e3b35cc2d1b802b646b9e
gjergjk71 <gjergjk71@gmail.com>
2018-10-21 18:26:42 +0200

    Changes on pressed behaviours of buttons

--- js/main.js
+++ js/main.js
@@ -51,7 +51,6 @@ settingsButton = new Button(ctx,undefined,300,250,250,"img/ui/buttons/settingsBu
 playButton = new Button(ctx,undefined,300,250,250,"img/ui/buttons/playButton.png");
 replayButton = new Button(ctx,undefined,370,125,125,"img/ui/buttons/replayButton.png");
 homeButton = new Button(ctx,undefined,undefined,125,125,"img/ui/buttons/homeButton.png");
-buttons = [playButton,infoButton,infoButton,playButton,replayButton,homeButton];
 var lastTime;
 var asteroids = [];
 var pickups = [];
@@ -59,6 +58,8 @@ var logo = new ImageClass(ctx,100,10,900,300,"img/ui/logo.png");
 
 game_scene = new Game(player,health,score,pickups,asteroids,gameOver,replayButton,homeButton);
 menu_scene = new Menu(canvas,logo,playButton,infoButton,settingsButton);
+credits_scene = new Credits(canvas,ctx,logo,homeButton);
+buttons = [playButton,infoButton,infoButton,playButton,replayButton,homeButton,credits_scene.leftArrowButton];
 
 function gameLoop(){
 	var now = Date.now();
@@ -68,17 +69,29 @@ function gameLoop(){
 	if (menu_scene.playButton.pressed){
 		game_scene.show = true;
 		menu_scene.playButton.pressed = false;
-	} else if (game_scene.homeButton.pressed){
+	} else if (homeButton.pressed){
 		game_scene.show = false;
-		menu_scene.show = true;
 		game_scene.reset;
-		game_scene.homeButton.pressed = false
+		homeButton.pressed = false;
+		menu_scene.show = true;
+	} 
+	if (credits_scene.leftArrowButton.pressed){
+		credits_scene.show = false;
+		menu_scene.show = true;
+		credits_scene.leftArrowButton.pressed = false;
+	}
+	if (infoButton.pressed){
+		menu_scene.show = false;
+		credits_scene.show = true;
+		infoButton.pressed = false;
 	}
 	if (game_scene.show){
 		game_scene.draw;
 		game_scene.dt = dt;
 	} else if (menu_scene.show) {
 		menu_scene.draw;
+	} else if (credits_scene.show){
+		credits_scene.draw;
 	}
 	lastTime = now;
 	window.requestAnimationFrame(gameLoop);
